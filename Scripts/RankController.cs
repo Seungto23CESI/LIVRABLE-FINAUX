f3f38d4400fea966b2b3d25b5a862f2fd20c2877
Noumi Rodrigue <62288209+Noumi-Rodrigue@users.noreply.github.com>
2020-12-16 03:34:28 +0100

    Update RankController.cs

--- Scripts/RankController.cs
+++ Scripts/RankController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class RankController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // Start is called before the first frame update.
 
     public GameManager gameManager;
 
