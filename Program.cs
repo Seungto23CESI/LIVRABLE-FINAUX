01ea8bf632158d7ac0c15afc20c89c9ad6b9e9c4
BABAN99 <75748703+BABAN99@users.noreply.github.com>
2020-12-16 08:36:49 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CAD;
+using middleware;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class_CAD obj = new Class_CAD();
+            Class_middleware mid = new Class_middleware();
+
+            string test = obj.get_cnx();
+            System.Console.WriteLine(test);
+
+            
+            System.Console.ReadLine();
+        }
+    }
+}
